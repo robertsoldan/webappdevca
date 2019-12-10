4c61a0d76fc47dfe08d4a759f9905074e0567309
robertsoldan <x18107800@student.ncirl.ie>
2019-12-10 23:12:09 +0000

    Comments section styled, some jquerry added

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,7 +1,6 @@
 function displayField(id) {
     $("#edit" + id).fadeOut(0);
     $("#" + id).fadeIn(500);
-    
 }
 
 function checkEmail(email, id) {
@@ -12,4 +11,13 @@ function checkEmail(email, id) {
         window.location.replace('/commentsedit/' + id);
         console.log("yea");
     } else alert("Sorry, this doesnt seem to be correct");
+}
+
+function showComment() {
+    $("#commentform").css("height", "1px").show().animate({height:"600px"}, 500);
+    $("#showcommentbtn").hide();
+}
+
+function submitForm() {
+    $("#commentform").animate({height:"0px"}, 2000)
 }
