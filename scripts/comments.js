1d559e7df55a159a3a6240aad91cc3b02e530136
robertsoldan <x18107800@student.ncirl.ie>
2019-12-11 00:32:00 +0000

    Input validation, more jquery, styling, picture

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,6 +1,6 @@
 function displayField(id) {
     $("#edit" + id).fadeOut(0);
-    $("#" + id).fadeIn(500);
+    $("#" + id).fadeIn(200);
 }
 
 function checkEmail(email, id) {
@@ -9,7 +9,6 @@ function checkEmail(email, id) {
 
     if(email === this.$("#emailcheck" + id).val()) {
         window.location.replace('/commentsedit/' + id);
-        console.log("yea");
     } else alert("Sorry, this doesnt seem to be correct");
 }
 
@@ -18,6 +17,34 @@ function showComment() {
     $("#showcommentbtn").hide();
 }
 
-function submitForm() {
-    $("#commentform").animate({height:"0px"}, 2000)
+function validateSubmitForm() {
+    var name = $("input[name=name]").val();
+    var email = $("input[name=email]").val();
+    var location = $("input[name=location]").val();
+
+    // Reset alerts
+    $("#name").text("");
+    $("#email").text("");
+    $("#location").text("");
+
+    // Validate first name contains only letters
+    if (/[0-9]/.test(name) || name === ''){
+        document.getElementById("name").innerHTML = "Input invalid.";
+    
+        // Validate email contains 
+    } else if (!(/@/.test(email))){
+        document.getElementById("email").innerHTML = "Email must contain '@'.";
+    
+    // Validate location name contains only letters
+    } else if (/[0-9]/.test(location) || location === ''){
+        document.getElementById("location").innerHTML = "Input invalid.";
+    
+    // Submission once all conditions evaluate FALSE
+    } else if($('textarea[name=comment]').val() === '') {
+        document.getElementById("comment").innerHTML = "Please leave a comment!";
+    } else {
+        document.forms["commentForm"].submit();
+        
+        return true;
+    }
 }
