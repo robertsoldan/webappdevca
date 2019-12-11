9f99bc5de6ef82cfe097f603e3784dcbde82f52c
Sophia767 <hennounisophi@yahoo.fr>
2019-12-11 00:06:35 +0000

    password added

--- scripts/password.js
+++ scripts/password.js
@@ -1,8 +1,8 @@
 function passwordCheck(){
-    var pw = prompt('STAFF ONLY\nPlease enter employee ID:');
-    if (pw === "0123456789A" || pw === "0123456789B" || pw === "0123456789C" || pw === "0123456789D"){
+    var pw = prompt("STAFF ONLY\nPlease enter employee ID:\n...but because this is only a project, let's just say password is:\nI love donuts!");
+    if (pw === "I love donuts!"){
         window.location.replace('/manage');
-    } else if (pw !== "0123456789A" || pw !== "0123456789B" || pw !== "0123456789C" || pw !== "0123456789D"){
+    } else {
         alert("Access denied.");
     }
 }
